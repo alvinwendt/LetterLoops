62abd174beb255413a9ac70613ff849fcb65d223
alvinwendt <alvinwendt@gmail.com>
2023-01-19 00:27:49 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Metrics;
+using System.Globalization;
+
+namespace LetterLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an input");
+            string input = Console.ReadLine();
+
+            //abcd
+            int Counter = 1
+                for ()
+            CultureInfo properCase = System.Threading.Thread.CurrentThread.CurrentCulture;
+            TextInfo currentInfo = properCase.TextInfo;
+            input = currentInfo.ToTitleCase(currentInfo.ToLower(input));
+            Console.WriteLine($"{input}");
+        }
+    }
+}
